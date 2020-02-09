8c1600b2ae27402efb8faa00ff5555127a552ad4
root <rnOOBaxbC1>
2020-02-09 07:31:54 +0200

    listener pakage

--- osanwego/tcp/server/listener.go
+++ osanwego/tcp/server/listener.go
@@ -7,20 +7,26 @@ import (
 	"net"
 )
 
-func Start() {
+func Start() error {
+	fmt.Println("Listener try start")
+
 	listener, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
 		log.Fatal("tcp server listener error:", err)
+		return err
 	}
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatal("tcp server accept error", err)
+			return err
 		}
 
 		go handleConnection(conn)
 	}
+
+	return nil
 }
 
 func handleConnection(conn net.Conn) {
