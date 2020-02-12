24e3d8ba6286219f6658423bb48a68ad39952bd9
root <rnOOBaxbC1>
2020-02-12 04:26:46 +0200

    x

--- osanwego/db/db.go
+++ osanwego/db/db.go
@@ -63,8 +63,8 @@ func GetSettings(key []byte) []byte {
 func SaveNode(node *protocol.Node) error {
 
 	xy := append(node.Pubkey.X, node.Pubkey.Y...)
-	fmt.Printf("PubKey: %v \n", node.Pubkey)
-	fmt.Printf("X+Y: %v \n", xy)
+	fmt.Printf("PubKey: %x \n", node.Pubkey)
+	fmt.Printf("X+Y: %x \n", xy)
 
 	hashNode := sha256.Sum256(xy)
 
