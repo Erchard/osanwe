63098ae8fca420b78b91535a556db528642234c4
root <rnOOBaxbC1>
2020-04-06 22:24:56 +0300

    Server start

--- osanwenet/cmd/server/main.go
+++ osanwenet/cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Erchard/osanwe/osanwenet/pb"
+	"github.com/Erchard/osanwe/osanwenet/service"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Server started")
+
+	helloServer := service.NewOsanweServer()
+	grpcServer := grpc.NewServer()
+	pb.RegisterHelloServiceServer(grpcServer, helloServer)
+
+	listener, err := net.Listen("tcp", "0.0.0.0:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
