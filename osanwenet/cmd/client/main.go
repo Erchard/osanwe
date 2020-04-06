53f7b51459ed658e623403bb9359110c076a35f3
root <rnOOBaxbC1>
2020-04-06 22:37:12 +0300

    Client

--- osanwenet/cmd/client/main.go
+++ osanwenet/cmd/client/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/Erchard/osanwe/osanwenet/pb"
+	"google.golang.org/grpc"
+	"log"
+)
 
 func main() {
 	fmt.Println("Client started")
+	conn, err := grpc.Dial("0.0.0.0:8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	helloClient := pb.NewHelloServiceClient(conn)
+
+	req := &pb.HelloRequest{
+		Msg: "Client is ok!",
+	}
+
+	res, err := helloClient.Hello(context.Background(), req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Response from server: %v \n", res)
+
 }
--- osanwenet/cmd/server/main.go
+++ osanwenet/cmd/server/main.go
@@ -15,11 +15,11 @@ func main() {
 	grpcServer := grpc.NewServer()
 	pb.RegisterHelloServiceServer(grpcServer, helloServer)
 
-	listener, err := net.Listen("tcp", "0.0.0.0:0")
+	listener, err := net.Listen("tcp", "0.0.0.0:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	fmt.Printf("Server started %v \n", listener.Addr())
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal(err)
