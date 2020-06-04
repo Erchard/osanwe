a0ecc32672ff3515cb76af2584bfc1dcaba79016
root <rnOOBaxbC1>
2020-06-04 07:45:35 +0300

    Connect with Auth

--- osanwebook/book/book.go
+++ osanwebook/book/book.go
@@ -1,12 +1,13 @@
 package book
 
-
 import (
 	"github.com/Erchard/osanwe/osanweauth/auth"
 	"github.com/Erchard/osanwe/osanweauth/pb"
 )
 
+var pub_key *pb.PubKey
 
-func init()  {
-	pub_key := auth.
+func init() {
+	pub_key = auth.GetMyPubkey()
+
 }
