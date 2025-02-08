8e38b2ecdd8dfe9f462e960a5b79a397d3ce6550
arsen <arsenguzhva@gmail.com>
2025-02-08 15:56:06 +0200

    DB config

--- osanwesrv/src/pgdb.rs
+++ osanwesrv/src/pgdb.rs
@@ -3,11 +3,15 @@ use tokio_postgres::{Error, NoTls};
 
 #[derive(serde::Deserialize)]
 struct DatabaseSettings {
-    url: String,
+    host: String,
+    port: String,
+    user: String,
+    password: String,
+    dbname: String,
 }
 
 pub async fn init_db() -> Result<(), Error> {
-    // Завантажуємо конфігурацію
+    // Завантаження конфігурації з файлу config.toml та змінного оточення
     let settings = Config::builder()
         .add_source(config::File::with_name("config"))
         .add_source(config::Environment::with_prefix("APP"))
@@ -18,7 +22,13 @@ pub async fn init_db() -> Result<(), Error> {
         .get::<DatabaseSettings>("database")
         .expect("Database settings not found");
 
-    let (client, connection) = tokio_postgres::connect(&db_settings.url, NoTls).await?;
+    // Формування рядка підключення із завантажених налаштувань
+    let connection_str = format!(
+        "host={} port={} user={} password={} dbname={}",
+        db_settings.host, db_settings.port, db_settings.user, db_settings.password, db_settings.dbname
+    );
+
+    let (client, connection) = tokio_postgres::connect(&connection_str, NoTls).await?;
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
